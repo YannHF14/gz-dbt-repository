60af2af00dcc88307a1550c345e8a73d39803520
Yann Hice <yannhice.filoche@gmail.com>
2025-07-30 10:52:06 +0000

    commit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -9,4 +9,5 @@ select
     round(s.quantity * p.purchase_price, 2) as purchase_cost,
     round(s.revenue - s.quantity * p.purchase_price, 2) as margin
 from {{ ref("stg_raw__sales") }} s
-left join {{ ref("stg_raw__product") }} p using (products_id)
+left join {{ ref("stg_raw__product") }} p 
+    using (products_id)
